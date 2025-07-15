3333a1fb31b5ba69c6101dcc079113b7ccd2dfa6
pjython <pjy@madup.com>
2025-07-15 14:28:40 +0900

    UserEventSubscription Base CRUD 추가

--- backend/app/crud/crud_users.py
+++ backend/app/crud/crud_users.py
@@ -1,10 +1,17 @@
 from app.crud.base import CRUDBase
-from app.models.users import Users
+from app.models.users import Users, UserEventSubscription
 from app.schemas.users import UsersCreate
 
 
 class CRUDUsers(CRUDBase[Users, UsersCreate, None]):
+    """사용자 CRUD 클래스"""
+    ...
+
+
+class CRUDUserEventSubscription(CRUDBase[UserEventSubscription, None, None]):
+    """사용자 이벤트 구독 CRUD 클래스"""
     ...
 
 
 crud_users = CRUDUsers(Users)
+crud_user_subscription = CRUDUserEventSubscription(UserEventSubscription)
